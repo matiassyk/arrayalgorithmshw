7cc07a9d0fa9b99562ab2532f875c0e02a197ff0
matiasmettala <uh@sjkksd.com>
2021-11-24 10:33:58 +0200

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -119,5 +119,25 @@ public class Main {
 //        array[ind1] = array[ind2];
 //        array[ind2] = tmp;
         
+       //#5
+//        final int[] arr = {9, 7, 2, 11};
+//        int n = arr.length;
+//        for (int i = 1; i <n; i++)
+//        {
+//            int val = arr[i];
+//            int j = i -1;
+//            while (j >= 0 && arr[j]> val)
+//            {
+//                arr[j + 1] = arr[j];
+//                j = j -1;
+//            }
+//
+//            {
+//                arr[j + 1] = val;
+//            }
+//        }
+//        for (int x : arr)
+//            System.out.print(x);
+
  }
 }
